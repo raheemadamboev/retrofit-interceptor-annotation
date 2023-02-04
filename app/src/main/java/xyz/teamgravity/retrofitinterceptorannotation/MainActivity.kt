c6d8c373caf2ac8e6a3618020f5d165af49a7f50
raheemadamboev <vividricemaryandmax@gmail.com>
2023-02-04 18:01:56 +0500

    Executed get requests;

--- app/src/main/java/xyz/teamgravity/retrofitinterceptorannotation/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/retrofitinterceptorannotation/MainActivity.kt
@@ -6,15 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import xyz.teamgravity.retrofitinterceptorannotation.ui.theme.RetrofitinterceptorannotationTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            RetrofitProvider.dummyApi.getUser()
+            RetrofitProvider.dummyApi.getPost()
+        }
+
         setContent {
             RetrofitinterceptorannotationTheme {
                 Surface(
